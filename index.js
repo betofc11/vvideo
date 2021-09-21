beebee05dd17026b9f5dead286bbc9354ee754ca
betofc11 <60046663+betofc11@users.noreply.github.com>
2021-09-21 01:44:29 -0600

    Finnaly

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 // HABILITAR BODYPARSER 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
