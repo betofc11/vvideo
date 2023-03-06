23ddf20a90457b9286adf5459dac1902707547e9
Luis Fonseca <lfonseca@pall.cl>
2023-03-06 17:48:06 -0600

    Revert "Fix session"
    This reverts commit 6128b805db4022e0b02ec626e0c97c627226778d.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 4000;
 // CREAR EL SERVIDOR 
 const app = express();
 
+module.exports.handler = serverless(app)
 
 // HABILITAR BODYPARSER 
 app.use((req, res, next) => {
@@ -29,9 +30,15 @@ app.use(express.urlencoded({ extended: true }));
 // ACTIVAR SESIONES
 app.use(session({
     secret: "GodNashee",
+    cookie: {
+        maxAge: 60000,
+        secure: true
+    },
     resave: false,
     saveUninitialized: true
 }))
+
+app.use('/.netlify/functions', routes())
 
 // RUTAS DE LA APP
 app.use('/', routes());
